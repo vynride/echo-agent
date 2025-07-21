14c27b6d7938b463a9bdb7011cbc6921f04cbc1b
Vivian <vynride@gmail.com>
2025-07-21 23:22:46 +0530

    ui: basics

---
+++ electron/main.js
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 425,
+    height: 650,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
---
+++ src/components/AgentProfile.jsx
@@ -0,0 +1,9 @@
+export default function AgentProfile() {
+  return (
+    <div className="flex items-center justify-center p-8">
+      <div>
+            <div className="rounded-full bg-[#0C6455] w-[250px] h-[250px] hover:shadow-[0px_0px_45px_0px_rgba(12,100,85,1)]"></div>
+      </div>
+    </div>
+  );
+}
