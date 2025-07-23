5b62715692a0e7db374e1192c631548953ee5180
Vivian <vynride@gmail.com>
2025-07-23 13:28:23 +0530

    feat: play audio after receiving response

--- backend/mic_on.py
+++ backend/mic_on.py
@@ -1,14 +1,18 @@
 from assembly import get_transcript
-from murf import gen_voiceover
+from murf import play_transcript
+
+def play_transcript(TRANSCRIPT, VOICEID, STYLE):
 
 try:
     transcript = get_transcript()
     print(f"Got Transcript from AssemblyAI\n {transcript}")
-
+    
+    print(f"Calling Murf for playing response")
     voice_id = "en-US-amara"
     style = "Conversational"
-
-    gen_voiceover(transcript, voice_id, style)
+    
+    play_transcript(transcript, voice_id, style)
+    print("Finished playing response")
     
 except Exception as e:
-    print(f"Error {e} occured when calling AssemblyAI for transcription.")
+    print(f"Error {e} occured")
